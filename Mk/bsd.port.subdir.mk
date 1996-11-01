33b8d3dce8f2a07fcefa2fd4d149dc68c6e19f2b
cvs2svn <cvs2svn@35697150-7ecd-e111-bb59-0022644237b5>
1996-11-01 07:22:38 +0000

    This commit was manufactured by cvs2svn to create tag 'RELENG_2_2_BP'.
    git-svn-id: svn+ssh://svn.freebsd.org/ports/tags/RELENG_2_2_BP@4233 35697150-7ecd-e111-bb59-0022644237b5

--- Mk/bsd.port.subdir.mk
+++ Mk/bsd.port.subdir.mk
@@ -1,5 +1,5 @@
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
-#	$Id: bsd.port.subdir.mk,v 1.8 1995/01/03 11:52:01 jkh Exp $
+#	$Id: bsd.port.subdir.mk,v 1.14 1996/04/09 22:54:13 wosch Exp $
 
 .MAIN: all
 
@@ -7,22 +7,34 @@
 STRIP?=	-s
 .endif
 
-BINGRP?=	bin
-BINOWN?=	bin
-BINMODE?=	555
+.if !defined(OPSYS)	# XXX !!
+OPSYS!=	uname -s
+.endif
+
+ECHO_MSG?=	echo
 
 _SUBDIRUSE: .USE
 	@for entry in ${SUBDIR}; do \
-		(if test -d ${.CURDIR}/$${entry}.${MACHINE}; then \
-			echo "===> ${DIRPRFX}$${entry}.${MACHINE}"; \
-			edir=$${entry}.${MACHINE}; \
-			cd ${.CURDIR}/$${edir}; \
-		else \
-			echo "===> ${DIRPRFX}$$entry"; \
-			edir=$${entry}; \
-			cd ${.CURDIR}/$${edir}; \
+		OK=""; \
+		for dud in $$DUDS; do \
+			if [ $${dud} = $${entry} ]; then \
+				OK="false"; \
+				${ECHO_MSG} "===> ${DIRPRFX}$${entry} skipped"; \
+			fi; \
+		done; \
+		if [ "$$OK" = "" ]; then \
+			if test -d ${.CURDIR}/$${entry}.${MACHINE}; then \
+				${ECHO_MSG} "===> ${DIRPRFX}$${entry}.${MACHINE}"; \
+				edir=$${entry}.${MACHINE}; \
+				cd ${.CURDIR}/$${edir}; \
+			else \
+				${ECHO_MSG} "===> ${DIRPRFX}$$entry"; \
+				edir=$${entry}; \
+				cd ${.CURDIR}/$${edir}; \
+			fi; \
+			${MAKE} ${.TARGET:realinstall=install} \
+				DIRPRFX=${DIRPRFX}$$edir/; \
 		fi; \
-		${MAKE} ${.TARGET:realinstall=install} DIRPRFX=${DIRPRFX}$$edir/); \
 	done
 
 ${SUBDIR}::
@@ -33,45 +45,12 @@ ${SUBDIR}::
 	fi; \
 	${MAKE} all
 
-.if !target(all)
-all: _SUBDIRUSE
+.for __target in all fetch fetch-list package extract configure \
+		 build clean depend describe reinstall tags checksum
+.if !target(__target)
+${__target}: _SUBDIRUSE
 .endif
-
-.if !target(fetch)
-fetch: _SUBDIRUSE
-.endif
-
-.if !target(package)
-package: _SUBDIRUSE
-.endif
-
-.if !target(extract)
-extract: _SUBDIRUSE
-.endif
-
-.if !target(configure)
-configure: _SUBDIRUSE
-.endif
-
-.if !target(build)
-build: _SUBDIRUSE
-.endif
-
-.if !target(clean)
-clean: _SUBDIRUSE
-.endif
-
-.if !target(depend)
-depend: _SUBDIRUSE
-.endif
-
-.if !target(describe)
-describe: _SUBDIRUSE
-.endif
-
-.if !target(reinstall)
-reinstall: _SUBDIRUSE
-.endif
+.endfor
 
 .if !target(install)
 .if !target(beforeinstall)
@@ -85,10 +64,49 @@ afterinstall: realinstall
 realinstall: beforeinstall _SUBDIRUSE
 .endif
 
-.if !target(tags)
-tags: _SUBDIRUSE
+.if !target(readmes)
+readmes: readme _SUBDIRUSE
 .endif
 
-.if !target(checksum)
-checksum: _SUBDIRUSE
+.if !target(readme)
+readme:
+	@rm -f README.html
+	@make README.html
 .endif
+
+.if (${OPSYS} == "NetBSD")
+PORTSDIR ?= /usr/opt
+.else
+PORTSDIR ?= /usr/ports
+.endif
+TEMPLATES ?= ${PORTSDIR}/templates
+.if defined(PORTSTOP)
+README=	${TEMPLATES}/README.top
+.else
+README=	${TEMPLATES}/README.category
+.endif
+
+README.html:
+	@echo "===>  Creating README.html"
+	@> $@.tmp
+.for entry in ${SUBDIR}
+.if defined(PORTSTOP)
+	@echo -n '<a href="'${entry}/README.html'">${entry}</a>: ' >> $@.tmp
+.else
+	@echo -n '<a href="'${entry}/README.html'">'"`cd ${entry}; make package-name`</a>: " >> $@.tmp
+.endif
+.if exists(${entry}/pkg/COMMENT)
+	@cat ${entry}/pkg/COMMENT >> $@.tmp
+.else
+	@echo "(no description)" >> $@.tmp
+.endif
+.endfor
+	@sort -t '>' +1 -2 $@.tmp > $@.tmp2
+	@cat ${README} | \
+		sed -e 's%%CATEGORY%%'`echo ${.CURDIR} | sed -e 's.*/\([^/]*\)$$\1'`'g' \
+			-e '/%%DESCR%%/r${.CURDIR}/pkg/DESCR' \
+			-e '/%%DESCR%%/d' \
+			-e '/%%SUBDIR%%/r$@.tmp2' \
+			-e '/%%SUBDIR%%/d' \
+		> $@
+	@rm -f $@.tmp $@.tmp2
