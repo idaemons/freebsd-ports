f37d2434ceea8828deb015e0f8e0567195bb3b99
cvs2svn <cvs2svn@35697150-7ecd-e111-bb59-0022644237b5>
1998-10-09 01:27:22 +0000

    This commit was manufactured by cvs2svn to create tag 'RELENG_3_0_0_RELEASE'.
    git-svn-id: svn+ssh://svn.freebsd.org/ports/tags/RELENG_3_0_0_RELEASE@13763 35697150-7ecd-e111-bb59-0022644237b5

--- Mk/bsd.port.subdir.mk
+++ Mk/bsd.port.subdir.mk
@@ -1,5 +1,37 @@
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
-#	$Id: bsd.port.subdir.mk,v 1.8 1995/01/03 11:52:01 jkh Exp $
+#	$Id: bsd.port.subdir.mk,v 1.23 1997/11/20 05:31:44 asami Exp $
+#
+# The include file <bsd.port.subdir.mk> contains the default targets
+# for building ports subdirectories. 
+#
+#
+# +++ variables +++
+#
+# STRIP		The flag passed to the install program to cause the binary
+#		to be stripped.  This is to be used when building your
+#		own install script so that the entire system can be made
+#		stripped/not-stripped using a single knob. [-s]
+#
+# ECHO_MSG	Used to print all the '===>' style prompts - override this
+#		to turn them off [echo].
+#
+# OPSYS		Get the operating system type [`uname -s`]
+#
+# SUBDIR	A list of subdirectories that should be built as well.
+#		Each of the targets will execute the same target in the
+#		subdirectories.
+#
+#
+# +++ targets +++
+#
+#	README.html:
+#		Creating README.html for package.
+#
+#	afterinstall, all, beforeinstall, build, checksum, clean,
+#	configure, deinstall, depend, describe, extract, fetch, fetch-list,
+#	install, package, readmes, realinstall, reinstall, tags
+#
+
 
 .MAIN: all
 
@@ -7,22 +39,35 @@
 STRIP?=	-s
 .endif
 
-BINGRP?=	bin
-BINOWN?=	bin
-BINMODE?=	555
+.if !defined(OPSYS)	# XXX !!
+OPSYS!=	uname -s
+.endif
+
+ECHO_MSG?=	echo
 
 _SUBDIRUSE: .USE
 	@for entry in ${SUBDIR}; do \
-		(if test -d ${.CURDIR}/$${entry}.${MACHINE}; then \
-			echo "===> ${DIRPRFX}$${entry}.${MACHINE}"; \
+		OK=""; \
+		for dud in $$DUDS; do \
+			if [ $${dud} = $${entry} ]; then \
+				OK="false"; \
+				${ECHO_MSG} "===> ${DIRPRFX}$${entry} skipped"; \
+			fi; \
+		done; \
+		if test -d ${.CURDIR}/$${entry}.${MACHINE}; then \
 			edir=$${entry}.${MACHINE}; \
-			cd ${.CURDIR}/$${edir}; \
-		else \
-			echo "===> ${DIRPRFX}$$entry"; \
+		elif test -d ${.CURDIR}/$${entry}; then \
 			edir=$${entry}; \
-			cd ${.CURDIR}/$${edir}; \
+		else \
+			OK="false"; \
+			${ECHO_MSG} "===> ${DIRPRFX}$${entry} non-existent"; \
 		fi; \
-		${MAKE} ${.TARGET:realinstall=install} DIRPRFX=${DIRPRFX}$$edir/); \
+		if [ "$$OK" = "" ]; then \
+			${ECHO_MSG} "===> ${DIRPRFX}$${edir}"; \
+			cd ${.CURDIR}/$${edir}; \
+			${MAKE} ${.TARGET:realinstall=install} \
+				DIRPRFX=${DIRPRFX}$$edir/; \
+		fi; \
 	done
 
 ${SUBDIR}::
@@ -33,45 +78,13 @@ ${SUBDIR}::
 	fi; \
 	${MAKE} all
 
-.if !target(all)
-all: _SUBDIRUSE
+.for __target in all fetch fetch-list package extract configure \
+		 build clean deinstall depend describe distclean \
+		 reinstall tags checksum
+.if !target(${__target})
+${__target}: _SUBDIRUSE
 .endif
-
-.if !target(fetch)
-fetch: _SUBDIRUSE
-.endif
-
-.if !target(package)
-package: _SUBDIRUSE
-.endif
-
-.if !target(extract)
-extract: _SUBDIRUSE
-.endif
-
-.if !target(configure)
-configure: _SUBDIRUSE
-.endif
-
-.if !target(build)
-build: _SUBDIRUSE
-.endif
-
-.if !target(clean)
-clean: _SUBDIRUSE
-.endif
-
-.if !target(depend)
-depend: _SUBDIRUSE
-.endif
-
-.if !target(describe)
-describe: _SUBDIRUSE
-.endif
-
-.if !target(reinstall)
-reinstall: _SUBDIRUSE
-.endif
+.endfor
 
 .if !target(install)
 .if !target(beforeinstall)
@@ -85,10 +98,84 @@ afterinstall: realinstall
 realinstall: beforeinstall _SUBDIRUSE
 .endif
 
-.if !target(tags)
-tags: _SUBDIRUSE
+IGNOREDIR=	CVS distfiles packages pkg templates
+
+.if !target(checksubdirs)
+.if defined(PORTSTOP)
+checksubdirs: checksubdir _SUBDIRUSE
+.else
+checksubdirs: checksubdir
+.endif
 .endif
 
-.if !target(checksum)
-checksum: _SUBDIRUSE
+.if !target(checksubdir)
+checksubdir:
+	@for d in *; do \
+	  if [ -d "$$d" ]; then \
+	    found=0; \
+	    for s in ${SUBDIR} ${IGNOREDIR}; do \
+	      if [ "x$$s" = "x$$d" ]; then \
+	        found=1; \
+	        break; \
+	      fi; \
+	    done; \
+	    if [ $$found = 0 ]; then \
+	      ${ECHO} "Warning: directory $$d not in SUBDIR"; \
+	    fi; \
+	  fi; \
+	done
 .endif
+
+.if !target(readmes)
+readmes: readme _SUBDIRUSE
+.endif
+
+.if !target(readme)
+readme:
+	@rm -f README.html
+	@make README.html
+.endif
+
+.if (${OPSYS} == "NetBSD")
+PORTSDIR ?= /usr/opt
+.else
+PORTSDIR ?= /usr/ports
+.endif
+TEMPLATES ?= ${PORTSDIR}/templates
+.if defined(PORTSTOP)
+README=	${TEMPLATES}/README.top
+.else
+README=	${TEMPLATES}/README.category
+.endif
+
+HTMLIFY=	sed -e 's/&/\&amp;/g' -e 's/>/\&gt;/g' -e 's/</\&lt;/g'
+
+README.html:
+	@echo "===>  Creating README.html"
+	@> $@.tmp
+.for entry in ${SUBDIR}
+.if defined(PORTSTOP)
+	@echo -n '<a href="'${entry}/README.html'">'"`echo ${entry} | ${HTMLIFY}`"'</a>: ' >> $@.tmp
+.else
+	@echo -n '<a href="'${entry}/README.html'">'"`cd ${entry}; make package-name | ${HTMLIFY}`</a>: " >> $@.tmp
+.endif
+.if exists(${entry}/pkg/COMMENT)
+	@${HTMLIFY} ${entry}/pkg/COMMENT >> $@.tmp
+.else
+	@echo "(no description)" >> $@.tmp
+.endif
+.endfor
+	@sort -t '>' +1 -2 $@.tmp > $@.tmp2
+.if exists(${.CURDIR}/pkg/DESCR)
+	@${HTMLIFY} ${.CURDIR}/pkg/DESCR > $@.tmp3
+.else
+	@> $@.tmp3
+.endif
+	@cat ${README} | \
+		sed -e 's/%%CATEGORY%%/'"`basename ${.CURDIR}`"'/g' \
+			-e '/%%DESCR%%/r$@.tmp3' \
+			-e '/%%DESCR%%/d' \
+			-e '/%%SUBDIR%%/r$@.tmp2' \
+			-e '/%%SUBDIR%%/d' \
+		> $@
+	@rm -f $@.tmp $@.tmp2 $@.tmp3
